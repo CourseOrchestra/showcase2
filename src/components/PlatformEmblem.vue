8e0d70aeafbc57bec14774133a3bc5ed40ab399e
Boris <toborto@yandex.ru>
2022-05-19 17:33:19 +0300

    Правки по эмблеме КУРСа

--- src/components/PlatformEmblem.vue
+++ src/components/PlatformEmblem.vue
@@ -1,22 +1,10 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <a
-          id="platform-emblem"
-          href="https://share.curs.ru/platformcourse/doc/1/general.html"
-          target="_blank"
-        >
-          <v-img
-            :src="require('../assets/kurs.png')"
-            class="my-3"
-            contain
-            height="200"
-          />
-        </a>
-      </v-col>
-    </v-row>
-  </v-container>
+  <a
+    href="https://share.curs.ru/platformcourse/doc/1/general.html"
+    target="_blank"
+  >
+    <v-img :src="require('../assets/kurs.png')" contain />
+  </a>
 </template>
 
 <script>
